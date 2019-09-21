8549f8e5eaec23f37a35deb8ba2fdf1ea320d871
vanshkapoor <vanshkapoorvk7@gmail.com>
2019-09-22 02:46:51 +0530

    added hooks

---
+++ r1/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import logo from './logo.svg';
+// import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import LandingPage from './components/landingPage';
+
+
+
+// const Parse = require('parse');
+// Parse.initialize("qd38MDMw9L37yUVcliBemAjSW6YosSXzNeOMH6C6",
+//   "SijHUPTfvwXJsCYWHGW86TTDUdi3qmYyxlWsvnUm");
+
+// Parse.serverURL = "https://aid.back4app.io";
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <Router >
+        <div className="App">
+        <Route exact path="/" component={LandingPage} />
+
+
+        </div>
+      </Router>
+    )
+  }
+}
+export default App;
---
+++ r1/src/components/landingPage.js
@@ -0,0 +1,21 @@
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+
+
+  return (
+    <div>
+
+      <div style={{marginTop:"50px"}}>
+     
+          <p>
+            Our goal is to provide guidance towards an informed deaddiction
+            programme.
+          </p>
+          </div>
+    </div>
+  );
+}
+
+export default LandingPage;
